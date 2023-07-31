171a667e373436a41483af5e1acd48cf137bac79
Marcone Reis <marcone.reis@voxline.com.br>
2023-07-31 16:27:44 -0300

    estrutura inicial

---
+++ Clientes/api/config/database.php
@@ -0,0 +1,9 @@
+<?php
+
+// Configurações do banco de dados
+$host = 'localhost';     // Endereço do banco de dados (normalmente, 'localhost')
+$dbname = 'nome_do_banco_de_dados'; // Nome do banco de dados
+$username = 'usuario_do_banco_de_dados'; // Nome de usuário do banco de dados
+$password = 'senha_do_banco_de_dados'; // Senha do banco de dados
+
+?>
